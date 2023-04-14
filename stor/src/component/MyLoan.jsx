640de5fb10b3efb24136f5b89644252bb8dcb8e8
Daniel dei <daniel.hajaj121@gmail.com>
2023-04-14 16:42:13 +0300

    build components of project

---
+++ stor/src/component/MyLoan.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyLoan() {
+  return <div>MyLoan</div>;
+}
---
+++ stor/src/component/OneProduct.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function OneProduct() {
+  return <div>OneProduct</div>;
+}
