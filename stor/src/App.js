3f02f70f5b8c864f2e13c128c64502b9ca903acd
Daniel dei <daniel.hajaj121@gmail.com>
2023-04-16 14:36:23 +0300

    "BSPMC2325-1 <page log in>"

--- stor/src/App.js
+++ stor/src/App.js
@@ -1,10 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Menu from "./component/Menu";
+import SignIn from "./component/SignIn";
+import NewLoan from "./component/NewLoan";
+import Product from "./component/Products";
+import MyLoan from "./component/MyLoan";
+import History from "./component/History";
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div className="App">
-      <h1>home page</h1>
+      <HashRouter>
+        <Menu></Menu>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/newloan" element={<NewLoan />} />
+          <Route path="/myloan" element={<MyLoan />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
--- stor/src/component/SignIn.jsx
+++ stor/src/component/SignIn.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      <h1>עמוד התחברות</h1>
+      <input
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        type="text"
+        placeholder="הזן שם משתמש/ת"
+      />
+      <br />
+      <input
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        type="text"
+        placeholder="הזן סיסמא"
+      ></input>
+      <button>כניסה</button>
+    </div>
+  );
 }
