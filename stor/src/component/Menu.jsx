4b88430b4ece14facb5112af16584d0b2155ffe6
Ora Sfez <ora@Air-de-Ora.lan>
2023-04-19 14:22:38 +0300

    BSPMC2325-36-1-26-34  admin changes user stories

--- stor/src/component/Menu.jsx
+++ stor/src/component/Menu.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 
 export default function Menu() {
-  return <div>Menu</div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div style={{ display: isLoggedIn ? "block" : "none" }}>
+      <Link to={"/products"}>
+        <button>מוצרים להשאלה</button>
+      </Link>
+
+      <Link to={"/newloan"}>
+        <button>טופס להשאלה חדשה</button>
+      </Link>
+      <Link to={"/myloan"}>
+        <button>השאלות שלי</button>
+      </Link>
+
+      <Link to={"/history"}>
+        <button>הסטוריית השאלות </button>
+      </Link>
+      <Link to={"/"}>
+        <button onClick={handleLogout}>התנתקות</button>
+      </Link>
+      <Link to={"/manager"}>
+        <button>מנהל</button>
+      </Link>
+    </div>
+  );
 }
--- stor/src/component/SignIn.jsx
+++ stor/src/component/SignIn.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function SignIn() {
-  return <div>SignIn</div>;
+export default function SignIn(props) {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const nav = useNavigate(); //כדי שכם יפעיל את הפונקציה שמעלה את התפריט וגם יעביר לעמוד של המשימות
+  return (
+    <div className="form">
+      <div className="form-style">
+        <h1>עמוד התחברות</h1>
+        <form>
+          <input
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            type="text"
+            placeholder="הזן שם משתמש/ת"
+          />
+          <label for="type"> :שם משתמש</label>
+
+          <br />
+          <br />
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="text"
+            placeholder="הזן סיסמא"
+          ></input>
+          <label for="type"> : סיסמא</label>
+
+          <div>
+            <button
+              className="buttonHome"
+              onClick={() => {
+                props.setShowMenu(true);
+                nav("/products");
+              }}
+            >
+              כניסה
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
