638b16907e7e71009dfaf550d71dc959bffca84e
AmitAvigdor <95162755+AmitAvigdor@users.noreply.github.com>
2023-04-20 19:15:44 +0300

    git commit -m "BSPMC2325-22 <message>"

--- stor/src/component/NewLoan.jsx
+++ stor/src/component/NewLoan.jsx
@@ -1,5 +1,58 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function NewLoan() {
-  return <div>NewLoan</div>;
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const nav = useNavigate(); //כדי שכם יפעיל את הפונקציה שמעלה את התפריט וגם יעביר לעמוד של המשימות
+  const [seqNum, setSeqNum] = useState("");
+  return (
+    <div className="form">
+      <div className="form-style">
+        <h1>השאלה חדשה</h1>
+        <input
+          onChange={(e) => {
+            setSeqNum(e.target.value);
+          }}
+          type="text"
+          placeholder="הכנס קוד מוצר: "
+        />
+        <label for="type">: הכנס קוד מוצר </label>
+        <br />
+        <br />
+        <form>
+          <input
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            type="text"
+            placeholder="dd/mm/yyyy"
+          />
+          <label for="type">: תאריך החזרה </label>
+
+          <br />
+          <br />
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="text"
+            placeholder="סיבת השאלה"
+          ></input>
+          <label for="type">: סיבת השאלה </label>
+          <div>
+            <button
+              className="buttonHome"
+              onClick={() => {
+                nav("/HomePage");
+              }}
+            >
+              לדף הבית
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
