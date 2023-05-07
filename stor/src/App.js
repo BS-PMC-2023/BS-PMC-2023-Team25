68e0a36adbd69526d6fd7064c8324d26bfd5e507
OraFit <orafi1@ac.sce.ac.il>
2023-05-07 23:18:20 +0300

    updates

--- stor/src/App.js
+++ stor/src/App.js
@@ -17,95 +17,7 @@ import Teacher from "./component/Teacher.jsx";
 
 function App() {
   const [showMenu, setShowMenu] = useState(false); //אחראי על הצגת התפריט
-  const [prod, setProduct] = useState([
-    //camera
-    { name: "camera", type: "Sony A7III", Snumber: "4476762", place: "true" },
-    { name: "camera", type: "Sony A7III", Snumber: "4477024", place: "true" },
-    { name: "camera", type: "Sony A7III", Snumber: "4477009", place: "true" },
-    { name: "camera", type: "Sony A7III", Snumber: "4476730", place: "true" },
-    { name: "camera", type: "Sony A7III", Snumber: "4476731", place: "true" },
-    { name: "camera", type: "Sony A7III", Snumber: "4476732", place: "true" },
-    { name: "camera", type: "Sony A7III", Snumber: "447673", place: "true" },
-    { name: "camera", type: "Sony A7III", Snumber: "4477025", place: "true" },
-    { name: "camera", type: "Sony A7III", Snumber: "4477008", place: "true" },
-    { name: "camera", type: "Sony A7III", Snumber: "4476733", place: "true" },
-
-    // ------
-    {
-      name: "camera",
-      type: "Panasonic DC-S5",
-      Snumber: "WJBA001375",
-      place: "true",
-    },
-    {
-      name: "camera",
-      type: "Panasonic DC-S5",
-      Snumber: "WJBA001396",
-      place: "true",
-    },
-    {
-      name: "camera",
-      type: "Panasonic DC-S5",
-      Snumber: "WJBA001397",
-      place: "true",
-    },
-    {
-      name: "camera",
-      type: "Panasonic DC-S5",
-      Snumber: "WJBA001400",
-      place: "true",
-    },
-    {
-      name: "camera",
-      type: "Panasonic DC-S5",
-      Snumber: "WJBA001399",
-      place: "true",
-    },
-    {
-      name: "camera",
-      type: "Panasonic DC-S5",
-      Snumber: "WJCA001283",
-      place: "true",
-    },
-    {
-      name: "camera",
-      type: "Panasonic DC-S5",
-      Snumber: "WJCA001273",
-      place: "true",
-    },
-    {
-      name: "camera",
-      type: "Panasonic DC-S5",
-      Snumber: "WJCA001275",
-      place: "true",
-    },
-    {
-      name: "camera",
-      type: "Panasonic DC-S5",
-      Snumber: "WJBA001411",
-      place: "true",
-    },
-    {
-      name: "camera",
-      type: "Panasonic DC-S5",
-      Snumber: "WJCA001289",
-      place: "true",
-    },
-
-    // ---
-    {
-      name: "camera",
-      type: "Sony A7III lens 105",
-      Snumber: "4663360",
-      place: "true",
-    },
-    {
-      name: "camera",
-      type: "Sony A7III lens 105",
-      Snumber: "4658456",
-      place: "true",
-    },
-  ]);
+  const [prod, setProduct] = useState([]);
 
   const addProduct = (n, t, sn, p) => {
     let temp = {
@@ -114,8 +26,33 @@ function App() {
       Snumber: sn,
       place: p,
     };
-    setProduct([temp, ...prod]);
+    fetch("/addProduct", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body: JSON.stringify({
+        prod: temp,
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setFlag(!flag);
+      });
   };
+
+  useEffect(() => {
+    fetch("/getData")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setProduct(data);
+      });
+  }, [flag]);
 
   const show = () => {
     //פונקציית הצגת התפריט
