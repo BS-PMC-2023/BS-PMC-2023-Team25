9217558b2ef4978da5af34ebdaf57494d80fd58d
Dan Monsonego <Danmo3@ac.sce.ac.il>
2023-05-23 13:31:50 +0300

    Fixing Bar duplicate and refactor orgenization

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,20 @@
 import express from "express";
 import cors from "cors";
-import products from "./api/products.routes.js";
+import productsRoutes from "./routes/products.routes.js";
 import bodyParser from "body-parser";
-import users from "./api/products.routes.js"
-import loans from "./api/products.routes.js"
+import usersRoutes from "./routes/users.routes.js";
+import loansRoutes from "./routes/loans.routes.js";
 
 const app = express();
 
 app.use(cors());
-
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use("/", products);
-<<<<<<< HEAD
-app.use("/users",users);
-app.use("/loans",loans);
-=======
-app.use("*", (req, res) => res.status(404).json({ error: "not found" })); //not defined page
->>>>>>> 0794715e699b25fbd46f412314b8760779019918
+app.use("/", productsRoutes);
+app.use("/users", usersRoutes);
+app.use("/loans", loansRoutes);
 
-app.use("*", (req, res) => res.status(404).json({ error: "not found" })); //not defined page
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+
 export default app;
