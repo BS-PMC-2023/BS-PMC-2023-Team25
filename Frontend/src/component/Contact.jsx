0efec7a805bc5357d05bf7504981ee83bb6ebe1b
AmitAvigdor <95162755+AmitAvigdor@users.noreply.github.com>
2023-05-30 14:48:27 +0300

    git commit -m "BSPMC2325-114 <Contact with Email>"

--- Frontend/src/component/Contact.jsx
+++ Frontend/src/component/Contact.jsx
@@ -1,72 +1,256 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import styled from "styled-components";
+// import React, { useState, useRef } from "react";
+// import emailjs from "emailjs-com";
 
-export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const navigate = useNavigate();
+// const Contact = () => {
+//   const form = useRef();
+//   const [isSent, setIsSent] = useState(false);
 
-  const handleSubmit = (e) => {
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs
+//       .sendForm(
+//         "service_nrwtpfg",
+//         "template_5v3fkhb",
+//         form.current,
+//         "jBg8aMVK0eA03GWRp"
+//       )
+//       .then(
+//         (result) => {
+//           console.log(result.text);
+//           console.log("message sent");
+//           setIsSent(true);
+//           setTimeout(() => {
+//             setIsSent(false);
+//           }, 2000); // Set the delay in milliseconds (2 seconds in this example)
+//         },
+//         (error) => {
+//           console.log(error.text);
+//         }
+//       );
+//   };
+
+//   return (
+//     <div className="form">
+//       <div className="form-style">
+//         <StyledContactForm>
+//           <h1>צור קשר</h1>
+//           <br />
+//           <form ref={form} onSubmit={sendEmail}>
+//             <label>שם מלא</label>
+//             <input type="text" name="user_name" />
+//             <label>דואר אלקטרוני</label>
+//             <input type="email" name="user_email" />
+//             <label>הודעה</label>
+//             <textarea name="message" />
+//             <input type="submit" value="שלח" />
+//             {isSent && <SentWindow>Sent</SentWindow>}
+//           </form>
+//         </StyledContactForm>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Contact;
+
+// // Styles
+// const StyledContactForm = styled.div`
+//   width: 400px;
+//   margin: 0 auto;
+
+//   form {
+//     display: flex;
+//     align-items: flex-start;
+//     flex-direction: column;
+//     width: 100%;
+//     font-size: 20px;
+
+//     input {
+//       width: 100%;
+//       height: 35px;
+//       padding: 7px;
+//       outline: none;
+//       border-radius: 55px;
+//       border: 1px solid rgb(220, 220, 220);
+
+//       &:focus {
+//         border: 2px solid rgba(0, 206, 158, 1);
+//       }
+//     }
+
+//     textarea {
+//       max-width: 100%;
+//       min-width: 100%;
+//       width: 100%;
+//       max-height: 100px;
+//       min-height: 100px;
+//       padding: 7px;
+//       outline: none;
+//       border-radius: 5px;
+//       border: 1px solid rgb(220, 220, 220);
+
+//       &:focus {
+//         border: 2px solid rgba(0, 206, 158, 1);
+//       }
+//     }
+
+//     label {
+//       margin-top: 1rem;
+//     }
+
+//     input[type="submit"] {
+//       margin-top: 2rem;
+//       cursor: pointer;
+//       background: rgb(249, 105, 14);
+//       color: white;
+//       border: none;
+//     }
+//   }
+// `;
+
+// const SentWindow = styled.div`
+//   position: absolute;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   padding: 10px;
+//   background-color: rgba(0, 0, 0, 0.8);
+//   color: white;
+//   font-size: 16px;
+//   border-radius: 5px;
+//   z-index: 9999;
+// `;
+
+import styled from "styled-components";
+import React, { useState, useRef } from "react";
+import emailjs from "emailjs-com";
+
+const Contact = () => {
+  const form = useRef();
+  const [isSent, setIsSent] = useState(false);
+
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    setName("");
-    setEmail("");
-    setMessage("");
-
-    navigate("/");
+    emailjs
+      .sendForm(
+        "service_nrwtpfg",
+        "template_5v3fkhb",
+        form.current,
+        "jBg8aMVK0eA03GWRp"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+          setIsSent(true);
+          setTimeout(() => {
+            setIsSent(false);
+          }, 2000);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
     <div className="form">
       <div className="form-style">
-        <h1>צור קשר</h1>
-        <br />
-        <form onSubmit={handleSubmit}>
-          <div className="form-field">
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="שם מלא"
-              style={{ textAlign: "center" }}
-            />
-            <label htmlFor="name">:הכנס/י את שמך</label>
-          </div>
+        <StyledContactForm>
+          <h1>צור קשר</h1>
           <br />
-          <div className="form-field">
+          <form ref={form} onSubmit={sendEmail}>
+            <label>שם מלא</label>
+            <input type="text" name="user_name" placeholder="הזן את שמך המלא" />
+
+            <label>דואר אלקטרוני</label>
             <input
               type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="כתובת דואר אלקטרוני"
-              style={{ textAlign: "center" }}
+              name="user_email"
+              placeholder="הזן כתובת דואר אלקטרוני"
             />
-            <label htmlFor="email">:הכנס/י דוא״ל </label>
-          </div>
-          <br />
-          <br />
-          <div className="form-field">
-            <textarea
-              id="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              placeholder="הודעה"
-              style={{ width: "100%", height: "150px", textAlign: "center" }}
-            ></textarea>
-            <label htmlFor="message">!נחזור אליך בהקדם</label>
-          </div>
-          <br />
-          <br />
-          <div>
-            <button className="buttonHome" type="submit">
-              שלח
-            </button>
-          </div>
-        </form>
+
+            <label>הודעה</label>
+            <textarea name="message" placeholder="הזן את ההודעה שלך"></textarea>
+
+            <input type="submit" value="שלח" />
+            {isSent && <SentWindow>Sent</SentWindow>}
+          </form>
+        </StyledContactForm>
       </div>
     </div>
   );
-}
+};
+
+export default Contact;
+
+// Styles
+const StyledContactForm = styled.div`
+  width: 400px;
+  margin: 0 auto;
+
+  form {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    width: 100%;
+    font-size: 20px;
+
+    input {
+      width: 100%;
+      height: 35px;
+      padding: 7px;
+      outline: none;
+      border-radius: 55px;
+      border: 1px solid rgb(220, 220, 220);
+
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
+
+    textarea {
+      max-width: 100%;
+      min-width: 100%;
+      width: 100%;
+      max-height: 100px;
+      min-height: 100px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
+
+    label {
+      margin-top: 1rem;
+    }
+
+    input[type="submit"] {
+      margin-top: 2rem;
+      cursor: pointer;
+      background: rgb(249, 105, 14);
+      color: white;
+      border: none;
+    }
+  }
+`;
+
+const SentWindow = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 10px;
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+  font-size: 16px;
+  border-radius: 5px;
+  z-index: 9999;
+`;
