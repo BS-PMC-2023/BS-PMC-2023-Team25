9d3cc697ab0da13f30f142e8aaff6db29c97be21
OraFit <orafi1@ac.sce.ac.il>
2023-06-05 13:26:31 +0300

    hackaton_25_problem5

--- Frontend/src/component/UserMenu.jsx
+++ Frontend/src/component/UserMenu.jsx
@@ -6,10 +6,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function UserMenu(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const email = localStorage.getItem("email");
 
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    props.setShowMenu(false);
+  const logoutUser = () => {
+    localStorage.removeItem("email");
   };
 
   return (
@@ -21,26 +21,26 @@ export default function UserMenu(props) {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <NavDropdown title="Loans" id="basic-nav-dropdown">
-            <Link to={"/newloan"} className="dropdown-item">
+            <Link to={`/newloan?email=${email}`} className="dropdown-item">
               New Loan
             </Link>
-            <Link to={"/myloan"} className="dropdown-item">
+            <Link to={`/myloan?email=${email}`} className="dropdown-item">
               My Loans
             </Link>
-            <Link to={"/history"} className="dropdown-item">
+            <Link to={`/history?email=${email}`} className="dropdown-item">
               Loan History
             </Link>
           </NavDropdown>
-          <Link to={"/podcast"} className="nav-link">
+          <Link to={`/podcast?email=${email}`} className="nav-link">
             Podcast Room
           </Link>
-          <Link to={"/student"} className="nav-link">
+          <Link to={`/student?email=${email}`} className="nav-link">
             Student
           </Link>
-          <Link to={"/contact"} className="nav-link">
+          <Link to={`/contact?email=${email}`} className="nav-link">
             Contact Us
           </Link>
-          <Link to={"/"} className="nav-link">
+          <Link to={"/"} className="nav-link" onClick={logoutUser}>
             Logout
           </Link>
         </Nav>
